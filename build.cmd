83800a2bf836e2c5fa5bb52d27ed9d236ececff1
Ryley Taketa <rtaket@microsoft.com>
2016-08-11 16:55:22 -0700

    add findbugs converter

--- build.cmd
+++ build.cmd
@@ -1,10 +1,16 @@
 SET OUTPATH=%~dp0\bld
 SET SRCPATH=%~dp0\src
 SET FINDBUGSPATH=%~dp0\tools\findbugs-3.0.1\bin
+SET CONVERTTOSARIFPATH=%~dp0\tools\ConvertToSarif
 
 IF EXIST %OUTPATH% RMDIR /Q /S %OUTPATH%
 MKDIR %OUTPATH%
 
-javac.exe -g -verbose -d %OUTPATH% %SRCPATH%\*.java
+ECHO Start compile
+CALL javac.exe -g -verbose -d %OUTPATH% %SRCPATH%\*.java
 
-%FINDBUGSPATH%\findbugs.bat -textui -effort:max -xml "%OUTPATH%\Hello.class" > %OUTPATH%\Hello.findbugs.xml
+ECHO Start analysis
+CALL %FINDBUGSPATH%\findbugs.bat -textui -effort:max -low -xml:withMessages -output %OUTPATH%\Hello.findbugs.xml "%OUTPATH%\Hello.class" 
+
+ECHO Start conversion
+CALL %CONVERTTOSARIFPATH%\ConvertToSarif.exe -t FindBugs -o bld\Hello.findbugs.converted.sarif -p -f bld\Hello.findbugs.xml
