e93c20442ef9fd1273d2b63917ab46250a49d483
Ryley Taketa <rtaket@microsoft.com>
2016-08-11 13:57:58 -0700

    run findbugs in build

--- build.cmd
+++ build.cmd
@@ -1,7 +1,10 @@
 SET OUTPATH=%~dp0\bld
 SET SRCPATH=%~dp0\src
+SET FINDBUGSPATH=%~dp0\tools\findbugs-3.0.1\bin
 
 IF EXIST %OUTPATH% RMDIR /Q /S %OUTPATH%
 MKDIR %OUTPATH%
 
 javac.exe -g -verbose -d %OUTPATH% %SRCPATH%\*.java
+
+%FINDBUGSPATH%\findbugs.bat -textui -effort:max -xml "%OUTPATH%\Hello.class" > %OUTPATH%\Hello.findbugs.xml
--- src/Hello.java
+++ src/Hello.java
@@ -3,4 +3,13 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.print("Hello World!");
 	}
+	
+    public static void incorrectAssignmentInIfCondition() {
+        boolean value = false;
+        if (value = true) {
+            //do Something
+        } else {
+            //else Do Something
+        }
+    }
 }
