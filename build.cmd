61af347676610975f6ff3a7de4715cabf0c7c567
Ryley Taketa <rtaket@microsoft.com>
2016-08-11 17:40:47 -0700

    run analysis on all files in the output directory.

--- build.cmd
+++ build.cmd
@@ -3,6 +3,12 @@ SET SRCPATH=%~dp0\src
 SET FINDBUGSPATH=%~dp0\tools\findbugs-3.0.1\bin
 SET CONVERTTOSARIFPATH=%~dp0\tools\ConvertToSarif
 
+IF '%APPVEYOR_PROJECT_NAME%' EQU '' (
+  SET PROJECTNAME=project
+) ELSE (
+  SET PROJECTNAME=%APPVEYOR_PROJECT_NAME%
+)
+
 IF EXIST %OUTPATH% RMDIR /Q /S %OUTPATH%
 MKDIR %OUTPATH%
 
@@ -10,7 +16,7 @@ ECHO Start compile
 CALL javac.exe -g -verbose -d %OUTPATH% %SRCPATH%\*.java
 
 ECHO Start analysis
-CALL %FINDBUGSPATH%\findbugs.bat -textui -effort:max -low -xml:withMessages -output %OUTPATH%\Hello.findbugs.xml "%OUTPATH%\Hello.class" 
+CALL %FINDBUGSPATH%\findbugs.bat -textui -effort:max -low -xml:withMessages -output %OUTPATH%\%PROJECTNAME%.findbugs.xml "%OUTPATH%" 
 
 ECHO Start conversion
-CALL %CONVERTTOSARIFPATH%\ConvertToSarif.exe -t FindBugs -o bld\Hello.findbugs.converted.sarif -p -f bld\Hello.findbugs.xml
+CALL %CONVERTTOSARIFPATH%\ConvertToSarif.exe -t FindBugs -o bld\%PROJECTNAME%.findbugs.converted.sarif -p -f bld\%PROJECTNAME%.findbugs.xml
