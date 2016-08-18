03c4046e8b652af6c577a6196e12cab89a61fc6f
Ryley Taketa <rtaket@microsoft.com>
2016-08-18 06:34:11 -0700

    Add comments to scripts.

--- build.cmd
+++ build.cmd
@@ -12,11 +12,24 @@ IF '%APPVEYOR_PROJECT_NAME%' EQU '' (
 IF EXIST %OUTPATH% RMDIR /Q /S %OUTPATH%
 MKDIR %OUTPATH%
 
-ECHO Start compile
+ECHO.
+ECHO.
+ECHO START COMPILATION:
+ECHO javac.exe -g -verbose -d %OUTPATH% %SRCPATH%\*.java
 CALL javac.exe -g -verbose -d %OUTPATH% %SRCPATH%\*.java
 
-ECHO Start analysis
+ECHO.
+ECHO.
+ECHO START ANALYSIS:
+ECHO %FINDBUGSPATH%\findbugs.bat -textui -effort:max -low -xml:withMessages -output %OUTPATH%\%PROJECTNAME%.findbugs.xml "%OUTPATH%" 
 CALL %FINDBUGSPATH%\findbugs.bat -textui -effort:max -low -xml:withMessages -output %OUTPATH%\%PROJECTNAME%.findbugs.xml "%OUTPATH%" 
 
-ECHO Start conversion
+ECHO.
+ECHO.
+ECHO START CONVERSION TO SARIF:
+ECHO %CONVERTTOSARIFPATH%\ConvertToSarif.exe -t FindBugs -o bld\%PROJECTNAME%.findbugs.converted.sarif -p -f bld\%PROJECTNAME%.findbugs.xml
 CALL %CONVERTTOSARIFPATH%\ConvertToSarif.exe -t FindBugs -o bld\%PROJECTNAME%.findbugs.converted.sarif -p -f bld\%PROJECTNAME%.findbugs.xml
+
+ECHO.
+ECHO.
+ECHO START SARIF URL REWRITE:
